bc855b7151aa49e9659286b8f5ca08e3e60a7336
idlefrenchfry <anakatrina1101@gmail.com>
2020-04-03 21:46:48 +0800

    Updates

---
+++ precache-manifest.a65ce5644ee74d8be87c6a2ff5c52a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c2ed017179cb12a33d53562cd71343",
+    "url": "/LIFE-React/index.html"
+  },
+  {
+    "revision": "d4d47c3c284fc226d592",
+    "url": "/LIFE-React/static/css/main.498a0658.chunk.css"
+  },
+  {
+    "revision": "50949edf9c3a4bde69cf",
+    "url": "/LIFE-React/static/js/2.fea23b3a.chunk.js"
+  },
+  {
+    "revision": "5002026598ed5d61cd3d68aaaa6f8e02",
+    "url": "/LIFE-React/static/js/2.fea23b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d47c3c284fc226d592",
+    "url": "/LIFE-React/static/js/main.302fbafd.chunk.js"
+  },
+  {
+    "revision": "39218969a1664ec6a398",
+    "url": "/LIFE-React/static/js/runtime-main.f7fa35c1.js"
+  }
+]);
