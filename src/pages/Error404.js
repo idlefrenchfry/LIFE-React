ef1eb23d0ca7a86e6d8223bb5f4b1a936b2aa1e8
idlefrenchfry <anakatrina1101@gmail.com>
2020-04-06 12:38:51 +0800

    Added error page

---
+++ src/pages/Error404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Error404() {
+    return (
+        <div className="card error">
+            <h1 className="sign"><span className="fast-flicker">E</span>rror<span className="flicker"> 4</span>04</h1>
+            <p>Seems like the page you've requested doesn't exist...</p>
+            <Link to="/">Go back to the dashboard</Link>
+        </div>
+    );
+}
+
+export default Error404;
