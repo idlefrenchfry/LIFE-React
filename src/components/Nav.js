3540c61a67862de890e81fb2c3e430f0565ee25f
idlefrenchfry <anakatrina1101@gmail.com>
2020-04-08 09:51:32 +0800

    Added active link styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../logo.png';
 
 function Nav() {
+    const [currentLoc, setCurrentLoc] = useState("");
+    useEffect(() => {
+        setCurrentLoc(window.location.href);
+    }, [window.location.href])
+
     return (
         <nav>
-            <Link to="/"><img src={logo} alt="Life" /></Link>
+            <NavLink to="/"><img src={logo} alt="Life" /></NavLink>
             <div className="navlinks">
-                <Link to="/">Dashboard</Link>
-                <Link to="/Members">Members</Link>
-                <Link to="/Coaches">Coaches</Link>
-                <Link to="/Events">Events</Link>
-                <Link to="/Trainings">Trainings</Link>
+                <NavLink exact={true} activeClassName='activelink' to="/">Dashboard</NavLink>
+                <NavLink activeClassName='activelink' to="/Members">Members</NavLink>
+                <NavLink activeClassName='activelink' to="/Coaches">Coaches</NavLink>
+                <NavLink activeClassName='activelink' to="/Events">Events</NavLink>
+                <NavLink activeClassName='activelink' to="/Trainings">Trainings</NavLink>
             </div>
         </nav>
     );
