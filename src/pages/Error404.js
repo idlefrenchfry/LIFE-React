065d0dacf2999e7707f92ddd8299a30e179ee8d1
idlefrenchfry <anakatrina1101@gmail.com>
2020-04-17 22:06:03 +0800

    Changed class name

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Error404() {
     return (
-        <div className="card error">
+        <div className="card errorPage">
             <h1 className="sign"><span className="fast-flicker">E</span>rror<span className="flicker"> 4</span>04</h1>
             <p>Seems like the page you've requested doesn't exist...</p>
             <Link to="/">Go back to the dashboard</Link>
