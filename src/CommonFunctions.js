413eb52eb332df14ddb8ec86e9a57cb5e83a3602
idlefrenchfry <anakatrina1101@gmail.com>
2020-04-29 15:53:44 +0800

    Added ISO string to Date function)

--- src/CommonFunctions.js
+++ src/CommonFunctions.js
@@ -5,8 +5,15 @@ function formatAMPM(date) {
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    let strTime = hours + ':' + minutes + ' ' + ampm;
+    let strTime = hours + ':' + minutes + ampm;
     return strTime;
 }
 
-export { formatAMPM };
+function ISOStringToDate(isostring) {
+    let dateInMili = Date.parse(isostring); // Parse ISO string to get miliseconds since Jan 1st 1970 00:00:00 GMT
+    let date = new Date(0);
+    date.setMilliseconds(dateInMili);
+    return date;
+}
+
+export { formatAMPM, ISOStringToDate};
