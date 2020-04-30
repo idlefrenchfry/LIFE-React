c0621a9e84b1908dd70a0907e91f85b7d3fe6e3c
idlefrenchfry <anakatrina1101@gmail.com>
2020-04-30 11:22:01 +0800

    Finished styling login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,18 +17,25 @@ function Login() {
     const handlePasswordChange = (e) => setPassword(e.target.value)
 
     return(
-        <div>
+        <div className="loginPage">
             <div className="sideimage">
-                <img src="https://images.unsplash.com/photo-1519053450113-32bed8bbf61d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80" alt=""/>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1519053450113-32bed8bbf61d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80" alt=""/>
+                </div>
 
             </div>
-            <div>
-                <label htmlFor="email"></label>
-                <input onChange={handleEmailChange} id="email" type="text"/>
-                <label htmlFor="password"></label>
-                <input onChange={handlePasswordChange} id="password" type="password"/>
-                <button onClick={forgotPassword}>Forgot Password</button>
-                <button onClick={login}>Login</button>
+            <div className="loginForm">
+                <div>
+                    <h1 style={{textAlign: "center"}}>Login</h1>
+                    <label htmlFor="email"></label>
+                    <input onChange={handleEmailChange} placeholder="Email" id="email" type="text"/>
+                    <label htmlFor="password"></label>
+                    <input onChange={handlePasswordChange} placeholder="Password" id="password" type="password"/>
+                    <button className="upperButton" onClick={forgotPassword}>Forgot Password</button>
+                    <div className="buttonsSet" style={{width: "100%"}}>
+                        <button className="loginButton" style={{width: "100%", margin: "0"}} onClick={login}>Login</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
