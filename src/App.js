ca97886f55a45fee4527451c35d0a40674baf89b
idlefrenchfry <anakatrina1101@gmail.com>
2020-05-04 14:52:05 +0800

    Added Edit pages

--- src/App.js
+++ src/App.js
@@ -36,6 +36,9 @@ function App() {
                     <Route path="/Coaches/Add" component={AddCoach}/>
                     <Route path="/Events/Add" component={AddEvent}/>
                     <Route path="/Training/Add" component={AddTraining} />
+                    <Route path="/Members/Edit/:id" component={AddMember}/>
+                    <Route path="/Coaches/Edit/:id" component={AddCoach}/>
+                    <Route path="/Events/Edit/:id" component={AddEvent}/>
                     <Route path="/Members/:id" exact component={MemberDetails} />
                     <Route path="/Coaches/:id" exact component={CoachDetails} />
                     <Route path="/Events/:id" exact component={EventDetails} />
