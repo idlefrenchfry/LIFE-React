f7cbe3b2e4f13f1342aa1cd35e184255cc0f2ed4
idlefrenchfry <anakatrina1101@gmail.com>
2020-05-12 16:10:50 +0800

    Added fontawesome icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,11 @@ function NavBar() {
         <nav>
             <NavLink to="/Dashboard"><img src="/logo.png" alt="Life" /></NavLink>
             <div className="navlinks">
-                <NavLink exact={true} activeClassName='activelink' to="/Dashboard">Dashboard</NavLink>
-                <NavLink activeClassName='activelink' to="/Members">Members</NavLink>
-                <NavLink activeClassName='activelink' to="/Coaches">Coaches</NavLink>
-                <NavLink activeClassName='activelink' to="/Events">Events</NavLink>
-                <NavLink activeClassName='activelink' to="/Trainings">Trainings</NavLink>
+                <NavLink exact={true} activeClassName='activelink' to="/Dashboard"><i class="fas fa-chart-bar" />Dashboard</NavLink>
+                <NavLink activeClassName='activelink' to="/Members"><i class="fas fa-user" />Members</NavLink>
+                <NavLink activeClassName='activelink' to="/Coaches"><i class="fas fa-user-tie" />Coaches</NavLink>
+                <NavLink activeClassName='activelink' to="/Events"><i class="fas fa-bookmark" />Events</NavLink>
+                <NavLink activeClassName='activelink' to="/Trainings"><i class="fas fa-dumbbell" />Trainings</NavLink>
             </div>
         </nav>
     );
