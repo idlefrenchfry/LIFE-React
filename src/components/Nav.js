37a57f8109f73c6243106984bce7b3af3ba49c7f
idlefrenchfry <anakatrina1101@gmail.com>
2020-05-18 09:57:02 +0800

    Fixed minor React warning

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,11 @@ function NavBar() {
         <nav>
             <NavLink to="/Dashboard"><img src="/logo.png" alt="Life" /></NavLink>
             <div className="navlinks">
-                <NavLink exact={true} activeClassName='activelink' to="/Dashboard"><i class="fas fa-chart-bar" />Dashboard</NavLink>
-                <NavLink activeClassName='activelink' to="/Members"><i class="fas fa-user" />Members</NavLink>
-                <NavLink activeClassName='activelink' to="/Coaches"><i class="fas fa-user-tie" />Coaches</NavLink>
-                <NavLink activeClassName='activelink' to="/Events"><i class="fas fa-bookmark" />Events</NavLink>
-                <NavLink activeClassName='activelink' to="/Trainings"><i class="fas fa-dumbbell" />Trainings</NavLink>
+                <NavLink exact={true} activeClassName='activelink' to="/Dashboard"><i className="fas fa-chart-bar" />Dashboard</NavLink>
+                <NavLink activeClassName='activelink' to="/Members"><i className="fas fa-user" />Members</NavLink>
+                <NavLink activeClassName='activelink' to="/Coaches"><i className="fas fa-user-tie" />Coaches</NavLink>
+                <NavLink activeClassName='activelink' to="/Events"><i className="fas fa-bookmark" />Events</NavLink>
+                <NavLink activeClassName='activelink' to="/Trainings"><i className="fas fa-dumbbell" />Trainings</NavLink>
             </div>
         </nav>
     );
